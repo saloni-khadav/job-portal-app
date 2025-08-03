3f079f9c19235b31fc36e53a322f658c0f90c92d
saloni-khadav <salonikhadav@gmail.com>
2025-08-03 12:12:45 +0530

    Initial commit

---
+++ backendcode/controllers/Webhooks.js
@@ -0,0 +1,96 @@
+// import { Webhook } from "svix";
+// import user from "../models/user.js";
+// export const clerkWebhooks=async(req,res)=>{
+//     try{
+//           const whook=new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+
+//           await whook.verify(JSON.stringify(req.body),{
+//             "svix-id":req.headers["svix-id"],
+//             "svix-timestamp":req.headers["svix-timestamp"],
+//             "svix-signature":req.headers["svix-signature"]
+//           })
+         
+//           const{data,type}=req.body
+
+//           switch(type){
+//             case 'user.created':{
+
+//                 const userData={
+//                     _id:data.id,
+//                     email:data.email_addresses[0].email_address,
+//                     name:data.first_name+ " "+data.last_name,
+//                     image:data.image_url,
+//                     resume:''
+//                 }
+
+//                 await user.create(userData)
+//                 res.json({})
+//                 break;
+
+//             }
+//              case 'user.updated':{
+//                 const userData={
+                   
+//                     email:data.email_addresses[0].email_address,
+//                     name:data.first_name+ " "+data.last_name,
+//                     image:data.image_url,
+                   
+//                 }
+//                 await user.findByIdAndUpdate(data.id,userData)
+//                  res.json({})
+//                 break;
+//             }
+//              case 'user.deleted':{
+//                 await user.findByIdAndDelete(data.id)
+//                 res.json({})
+//                 break;
+//             }
+//             default:
+//                 break;
+//           }
+
+//     }catch(error){
+//         console.log(error.message);
+//         res.json({success:false,message:'webhooks error'})
+
+//     }
+// }
+
+
+
+import { Webhook } from 'svix';
+import user from '../models/user.js';
+
+export const clerkWebhooks = async (req, res) => {
+  try {
+    const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+    const evt = wh.verify(
+      JSON.stringify(req.body),
+      {
+        'svix-id': req.headers['svix-id'],
+        'svix-timestamp': req.headers['svix-timestamp'],
+        'svix-signature': req.headers['svix-signature']
+      }
+    );
+
+    const { data, type } = evt;
+    console.log('Webhook Type:', type);  
+    console.log('Webhook Data:', data);  
+
+    if (type === 'user.created') {
+      await user.create({
+        _id: data.id,
+        name: `${data.first_name} ${data.last_name}`,
+        email: data.email_addresses[0].email_address,
+        image: data.image_url,
+        resume: ''
+      });
+    }
+
+    res.json({ received: true });
+  } catch (err) {
+    console.error('Webhook Error:', err.message);
+    res.status(400).json({ success: false });
+  }
+};
--- backendcode/index.js
+++ backendcode/index.js
@@ -3,66 +3,54 @@
 // import cors from 'cors';
 // import dotenv from 'dotenv';
 // import connectDB from './utils/db.js';
-// import bodyParser from 'body-parser';
-// import user from './models/user.js';
 
 // import { clerkWebhooks } from './controllers/Webhooks.js';
 // dotenv.config({})
 // const app=express();
 
-// app.post('/Webhooks',
-//   bodyParser.raw({ type: 'application/json' }), // raw body only for this route
-//   clerkWebhooks
-// );
+
 
 // app.use(express.json());
-
+// app.post('/Webhooks',clerkWebhooks)
 // app.use(express.urlencoded({extended:true}));
 // app.use(cookieParser());
 
 // const corsOptions={
-//     origin:'http://Localhost:5173',
+//     origin:'http//Localhost:5173',
 //     Credentials:true
 // }
 // app.use(cors(corsOptions));
 
 // const PORT=process.env.PORT|| 3000;
-
-// const startServer = async () => {
-//   await connectDB();
-//   app.listen(PORT, () => {
-//     console.log(`Server running at port ${PORT}`);
-//   });
-// };
+// app.listen(PORT,()=>{
+//     connectDB();
+// console.log(`server running at port ${PORT}`);
+// })
 
 
 
-
-
-// startServer();
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './utils/db.js';
 import { clerkWebhooks } from './controllers/Webhooks.js';
-import bodyParser from 'body-parser'; // 👈 ADD THIS
+import bodyParser from 'body-parser'; 
 import user from "./models/user.js";
 
 dotenv.config();
 
 const app = express();
 
-// 👇 This MUST come BEFORE express.json()
-// app.post('/Webhooks', bodyParser.raw({ type: 'application/json' }), clerkWebhooks);
+
 app.post('/Webhooks', clerkWebhooks);
-// Regular middleware AFTER webhook
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: 'http://localhost:5173', // 🔧 fix typo: http// → http://
+  origin: 'http://localhost:5173', 
   credentials: true
 };
 app.use(cors(corsOptions));
